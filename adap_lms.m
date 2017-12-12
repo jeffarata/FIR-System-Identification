9aa6d4e6ff12de0060511faf74dbae0c3fa5936c
jka <jeffarata@comcast.net>
2017-12-11 19:01:15 -0600

    updating adaptive filter as well as script testing them

--- adap_lms.m
+++ adap_lms.m
@@ -2,7 +2,7 @@
 % This function is an implementation of an LMS adaptive filter using
 % information gleaned from the wikipedia article on the filter.
 
-function [e, h] = adap_lms( x, d, mu, order )
+function [h, e, y] = adap_lms( x, d, mu, order )
 % This function is an adaptive filter using an LMS algorithm.
 %
 % x     - is the input signal
@@ -12,16 +12,19 @@ function [e, h] = adap_lms( x, d, mu, order )
 %
 % h     - the filter coefficients
 % e     - the error found
+% y     - the filtered output signal
 
 x = x(:);               % Make sure x is a column vector
 N = length(x);          
 
-h = zeros(order+1, 1);  % Initialize filter coefficients to be 0
+h = zeros(order, 1);    % Initialize filter coefficients to be 0
 e = zeros(1,N);         % Initialize error vector for efficiency
+y = zeros(N,1);         % Initialize output
 
-for ii = order+1:N 
-    x_col = x(ii:-1:ii-order);          % Slice of x we want  
+for ii = order:N 
+    x_col = x(ii:-1:ii-order+1);        % Slice of x we want  
     e(ii) = d(ii) - h' * x_col;         % Error at current time of iith sample
+    y(ii) = h' * x_col;                 % Update output
     h = h + mu * conj(e(ii)) * x_col;   % Update the filter weights.
 end
 
--- inverse_system_id.m
+++ inverse_system_id.m
@@ -9,6 +9,7 @@
 
 clear;
 clc;
+close all;
 
 
 [clean_oboe, fs] = audioread('oboe.wav');
@@ -16,28 +17,33 @@ N = length(clean_oboe);
 
 order = 128;                        % Order of filter
 cutoff = 2500;                      % Cutoff frequency of filter
-b = fir1(order-1, cutoff/(fs/2));   % Coefficients of generated filter, lowpass
-noise = 0.1*randn(N, 1);            % Noise to add to oboe
+b = fir1(order-1, cutoff/(fs/2));     % Coefficients of generated filter, lowpass
+noise = 0.05*randn(N, 1);            % Noise to add to oboe
 fuzzy_oboe = filter(b, 1, clean_oboe) + noise;  % Our unknown system x[n]
 
 % Remember that generally with higher order filters, you'll want a smaller
 % step size, mu.
-mu = 0.008;                         % LMS step size
-h = adaptfilt.lms( order, mu );     % The LMS filter
+mu = .008;                          % LMS step size
+%mu = 1;                             % NLMS step size
+%h = adaptfilt.lms( order+1, mu );     % The LMS filter, matlab bultin
 
-[y, e] = filter(h, fuzzy_oboe, clean_oboe); % Output and Error of lms filter
+% My own LMS adaptive filter implementation
+[h, my_error, my_y] = adap_nlms( fuzzy_oboe, clean_oboe, mu, order);
+
+%[y, e] = filter(h, 1, fuzzy_oboe);
 
 % Plot the clean oboe, the filtered signal (output), and the error.
 figure(1)
-title('Inverse System Identification')
 subplot(2, 1, 1)
-plot([clean_oboe, y])
+plot([clean_oboe, my_y])
 axis([0 9000 -1 1])
+title('Inverse System Identification')
 ylabel('Amplitude')
 xlabel('Sample')
 legend('Clean Oboe d[n]', 'Output y[n]')
 subplot(2, 1, 2)
-plot(e, 'r')
+%plot(e, 'r')
+plot(my_error, 'r')
 axis([0 9000 -1 1])
 ylabel('Amplitude')
 xlabel('Sample')
@@ -48,17 +54,21 @@ legend('Error e[n]')
 [PSD_desired, F] = pwelch(clean_oboe, [], [], [], fs);
 logPSD_desired = 10*log10(PSD_desired);                    
      
-[PSD_output, F] = pwelch(y, [], [], [], fs);
+[PSD_output, F] = pwelch(my_y, [], [], [], fs);
 logPSD_output = 10*log10(PSD_output); 
 
-[PSD_error, F] = pwelch(e, [], [], [], fs);
+%[PSD_error, F] = pwelch(e, [], [], [], fs);
+%logPSD_error = 10*log10(PSD_error); 
+
+[PSD_error, F] = pwelch(my_error, [], [], [], fs);
 logPSD_error = 10*log10(PSD_error); 
 
+
 figure(2)
-title('Power Spectral Density')
 subplot(2, 1, 1)
 plot(F, [logPSD_desired, logPSD_output])
 axis([0 4500 -100 -10])
+title('Power Spectral Density')
 ylabel('dB')
 xlabel('Frequency (Hz)')
 legend('Clean Oboe', 'Output')
@@ -69,19 +79,37 @@ ylabel('dB')
 xlabel('Frequency (Hz)')
 legend('Error')
 
+%{
+figure(2)
+plot(F, [logPSD_desired, logPSD_output, logPSD_error])
+axis([0 4500 -100 -10])
+title('Power Spectral Density')
+ylabel('dB')
+xlabel('Frequency (Hz)')
+legend('Clean Oboe', 'Output', 'Error')
+%}
+
 % Plot the filter coefficients of our unknown system (before noise) and the
 % coefficients of the adaptive lms filter
 
 figure(3)
+%stem([b' h.Coefficients'])
+stem([b' h])
 title('Filter Coefficients')
-stem([b' h.Coefficients'])
 ylabel('Value')
 xlabel('Coefficient Number')
 legend('Unknown System', 'Adaptive Filter')
 
 % Playback the clean desired oboe, the filtered output oboe, and the error
+fprintf('Playing clean oboe\n')
 sound(clean_oboe, fs)
 pause(2)
-sound(y, fs)
+fprintf('Playing fuzzy oboe signal\n')
+sound(fuzzy_oboe, fs)
 pause(2)
-sound(e, fs)
+fprintf('Playing filtered oboe\n')
+sound(my_y, fs)
+pause(2)
+fprintf('Playing error signal\n')
+%sound(e, fs)
+sound(my_error, fs)
